60dca6ba9b22829551b08ba29ef7ea29f506aac8
Igorik <egorsborsikov250575@gmail.com>
2025-06-11 19:01:23 +0700

    add new tabitem with Score table

---
+++ EditorControls/ScoreTable/ScoreTable.axaml.cs
@@ -0,0 +1,27 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace ProtocolEditor.EditorControls.ScoreTable;
+
+public partial class ScoreTable : UserControl
+{
+    public ScoreTable()
+    {
+        InitializeComponent();
+    }
+
+    private void AddScoreEntry_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void RemoveScoreEntry_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void ExportScoreTableToExcel_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
